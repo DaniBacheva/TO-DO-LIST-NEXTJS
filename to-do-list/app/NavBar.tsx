923b4eb68de7a718d8171a72317202e2878d87a1
DaniBacheva <y.bacheva@abv.bg>
2024-05-21 22:15:05 +0300

    refactoring navbar code

--- to-do-list/app/NavBar.tsx
+++ to-do-list/app/NavBar.tsx
@@ -1,18 +1,25 @@
+'use client'
+
 import Link from 'next/link'
 import React from 'react'
 import { FcTodoList } from "react-icons/fc";
 
 const NavBar = () => {
+
+    const links = [
+        { name: 'Dashboard', href: '/' },
+        { name: 'To-do-list', href: '/toDos' }
+    ]
     return (
         <nav className='flex space-x-7 border-b-2 mb-5 px-4 h-12 items-center'>
             <Link href="/"><FcTodoList /></Link>
             <ul className='flex space-x-6'>
-                <li>
-                    <Link className='text-blue-500 hover:text-blue-800 transition-colors' href="/">Dashboard</Link>
-                </li>
-                <li>
-                    <Link className='text-blue-500 hover:text-blue-800 transition-colors' href="/toDos">To-do-list</Link>
-                </li>
+                {links.map(link =>
+                    <Link
+                        key={link.href}
+                        className='text-blue-500 hover:text-blue-800 transition-colors'
+                        href={link.href}>{link.name}</Link>)}
+
             </ul>
         </nav>
 
