aa24b6cbdb6216a71845f8b4e7ce5b496d69d466
DaniBacheva <y.bacheva@abv.bg>
2024-05-24 17:53:13 +0300

    Set up Radix UI

--- to-do-list/app/toDos/page.tsx
+++ to-do-list/app/toDos/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Button } from '@radix-ui/themes'
 
 const ToDoPage = () => {
   return (
-    <div>ToDoPage</div>
+    <div>
+      <Button>New Issue</Button>
+    </div>
   )
 }
 
