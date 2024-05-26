197250eceec74f2be335326fd973e731d5027f73
DaniBacheva <y.bacheva@abv.bg>
2024-05-26 18:10:30 +0300

    Handle form submition

--- to-do-list/app/toDos/new/page.tsx
+++ to-do-list/app/toDos/new/page.tsx
@@ -1,19 +1,40 @@
 'use client';
 
-import { Button,  TextField } from '@radix-ui/themes'
-
+import { Button, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
 import "easymde/dist/easymde.min.css";
+import { useRouter } from 'next/navigation';
+
+interface ToDoForm {
+  title: string;
+  description: string
+}
 
 const NewTodoPage = () => {
-  return (
-    <div className='max-w-xl space-y-4'>
-        <TextField.Root placeholder='Title' />
-      
-        <SimpleMDE placeholder="Description" />
+  const router = useRouter();
 
-        <Button>Submit New Issue</Button>
-        </div>
+  const { register, control, handleSubmit } = useForm<ToDoForm>();
+  // console.log(register('title'))
+
+  return (
+    <form
+      className='max-w-xl space-y-4'
+      onSubmit={handleSubmit(async(data) => {
+        await axios.post('/api/toDos', data);
+        router.push('/toDos');
+        })}>
+      <TextField.Root placeholder='Title' {...register('title')} />
+
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+      />
+
+      <Button>Submit New Issue</Button>
+    </form>
   )
 }
 
